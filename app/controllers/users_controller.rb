bdb6fbf30cf3c403cd388791d2664981424fad25
Tasreen Mowreen <tmowreen97@gmail.com>
2023-01-29 21:37:17 -0500

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+    # else
+    #   render json: {error: "Not found"}, status: :not_found
+    # end
   end
 
 end
