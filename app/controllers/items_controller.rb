42498eef011110794a1bde6035c65a1db243f2d9
Tasreen Mowreen <tmowreen97@gmail.com>
2023-01-29 21:36:49 -0500

    commit tree 33797b7a5e8dd60e8a5318e4b16c2377edc8a840

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,42 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+        items = user.items 
+        render json: items, status: :ok
+      else
+        render json: {error: "Not found"}, status: :not_found
+      end
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
   end
+
+  def show
+    item = Item.find_by(id:params[:id])
+    if item
+      render json: item, status: :ok
+    else
+      render json: { error: "Not found"}, status: :not_found
+    end
+      
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    new_item = user.items.create(item_params)
+    render json: new_item, status: :created
+  end
+  #"name"=>"Garden gnomes", "description"=>"No refunds", "price"=>23,
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
 
 end
